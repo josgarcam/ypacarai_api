bd47f33dfedc0891f293b14bd57a4c9081d96e40
jmgarciac <jmgarciac@ayesa.es>
2021-03-12 14:47:13 +0100

    Añadida ruta al servidor de Paraguay, se necesita vpn activada, e instrucciones para correr el código desde el cmd

--- controller/main.py
+++ controller/main.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python3
+import sys
+import os
+PACKAGE_PARENT = '..'
+SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd(), os.path.expanduser(__file__))))
+sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+
 from flask import Flask, request
 from model.navegacion import navegacion_all, navegacion_drone_id_and_date
 from model.drones import drones_all, drones_id
--- model/db.py
+++ model/db.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('postgresql://postgres:admin@localhost:5432/paraguay_jose')
+# engine = create_engine('postgresql://postgres:postgres@192.168.20.21:5432/FlotaDrones')
 Session = sessionmaker(bind=engine)
 session = Session()
 
