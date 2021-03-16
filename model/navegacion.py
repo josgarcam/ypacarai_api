71af65de22f6268d0e5edcace098edb872c61a91
AYESA\arocha <arocha@ayesa.com>
2021-03-16 13:41:29 +0100

    Se incluyen season y deployment en la salida

--- model/navegacion.py
+++ model/navegacion.py
@@ -6,15 +6,17 @@ from model import db
 
 def navegacion_all():
     db.Base.metadata.create_all(db.engine)
-    ob = db.session.query(Navegacion).all()
+    ob = db.session.query(Navegacion, Seasons).join(Seasons, Navegacion.nav_id_movement == Seasons.sea_id_movement).all()
     data = {}
     i = 0
     for obj in ob:
-        data[i] = {'drone_id': obj.nav_id_drone,
-                   'latitude': obj.nav_latitude,
-                   'longitude': obj.nav_longitude,
-                   'altitude': obj.nav_altitude,
-                   'date': obj.nav_date}
+        data[i] = {'season': obj.Seasons.sea_season_id,
+                   'deployment': obj.Seasons.sea_deployment_id,
+                   'id_drone': obj.Navegacion.nav_id_drone,
+                   'latitude': obj.Navegacion.nav_latitude,
+                   'longitude': obj.Navegacion.nav_longitude,
+                   'altitude': obj.Navegacion.nav_altitude,
+                   'date': obj.Navegacion.nav_date}
         i += 1
 
     return (data)
@@ -23,7 +25,8 @@ def navegacion_all():
 def navegacion_drone_id_and_date(drone_id, sdate, edate):
 
     if sdate and edate:
-        ob = db.session.query(Navegacion).filter(Navegacion.nav_id_drone == drone_id). \
+        ob = db.session.query(Navegacion, Seasons).join(Seasons, Navegacion.nav_id_movement == Seasons.sea_id_movement).\
+            filter(Navegacion.nav_id_drone == drone_id). \
             filter(Navegacion.nav_date >= datetime.strptime(sdate, '%m-%d-%y')). \
             filter(Navegacion.nav_date <= datetime.strptime(edate, '%m-%d-%y')) \
             .order_by(Navegacion.nav_date.asc())
@@ -34,10 +37,12 @@ def navegacion_drone_id_and_date(drone_id, sdate, edate):
     data = {}
     i = 0
     for obj in ob:
-        data[i] = {'latitude': obj.nav_latitude,
-                   'longitude': obj.nav_longitude,
-                   'altitude': obj.nav_altitude,
-                   'date': obj.nav_date}
+        data[i] = {'season': obj.Seasons.sea_season_id,
+                   'deployment': obj.Seasons.sea_deployment_id,
+                   'latitude': obj.Navegacion.nav_latitude,
+                   'longitude': obj.Navegacion.nav_longitude,
+                   'altitude': obj.Navegacion.nav_altitude,
+                   'date': obj.Navegacion.nav_date}
         i += 1
 
     return (data)
@@ -64,12 +69,11 @@ def navegacion_season(season, deployment, drone_id):
     data = {}
     i = 0
     for obj in ob:
-        data[i] = {'date': obj.Navegacion.nav_date,
-                   'latitude': obj.Navegacion.nav_latitude,
-                   'altitude': obj.Navegacion.nav_altitude,
-                   'longitude': obj.Navegacion.nav_longitude,
+        data[i] = {'deployment': obj.Seasons.sea_deployment_id,
                    'id_drone': obj.Navegacion.nav_id_drone,
-                   'deployment': obj.Seasons.sea_deployment_id,
-                   }
+                   'latitude': obj.Navegacion.nav_latitude,
+                   'longitude': obj.Navegacion.nav_longitude,
+                   'altitude': obj.Navegacion.nav_altitude,
+                   'date': obj.Navegacion.nav_date}
         i += 1
     return (data)
